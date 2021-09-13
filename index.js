1737d86efc630500077c939c68cba1d683e2ba51
Jeff Hykin <jeff.hykin@gmail.com>
2021-09-13 18:39:57 -0500

    add python and js version

---
+++ index.js
@@ -0,0 +1,23 @@
+const process = require("process")
+const path = require("path")
+const fs = require("fs")
+
+const walkUpUntil = (fileToFind, startPath=null)=> {
+    let here = startPath || process.cwd()
+    if (!path.isAbsolute(here)) {
+        here = path.join(process.cwd(), fileToFind)
+    }
+    while (1) {
+        let checkPath = path.join(here, fileToFind)
+        if (fs.exists(checkPath)) {
+            return checkPath
+        }
+        // reached the top
+        if (here == path.dirname(here)) {
+            return null
+        } else {
+            // go up a folder
+            here =  path.dirname(here)
+        }
+    }
+}
